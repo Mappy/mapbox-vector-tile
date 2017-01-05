e4660e719e99439dd64ad5616063f7f67483fa3f
Lexman <alexandre.bonnasseau@gmail.com>
2017-01-05 13:55:55 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def test_suite():
     return suite
 
 setup(name='mapbox-vector-tile-mappy',
-      version='1.0.1',
+      version='1.0.2',
       description=u"Mapbox Vector Tile",
       long_description=long_description,
       classifiers=[],
