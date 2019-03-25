ab8f4f87da9e2f9cd38b2ecc3dbeaa3732a84254
Stephen KINGER <stephen.kinger@mappy.com>
2019-03-25 13:57:53 +0100

    add p3 compatibility with protobuf3 and upgrade version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def test_suite():
     return suite
 
 setup(name='mapbox-vector-tile-mappy',
-      version='1.0.6',
+      version='1.0.7',
       description=u"Mapbox Vector Tile",
       long_description=long_description,
       classifiers=[],
